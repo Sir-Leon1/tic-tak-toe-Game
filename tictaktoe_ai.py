9e6ef0b78a0e9b673404aa2b0deccbea24cdb22d
Sir-Leon1 <eb3.61594.22@student.chuka.ac.ke>
2023-05-16 23:17:05 +0300

    First final

--- tictaktoe_ai.py
+++ tictaktoe_ai.py
@@ -38,8 +38,43 @@ def user_2_turn(board):
         exit(0)
     board[pos - 1] = 1
 
+def minmax(board, player):
+    x = analyze_board(board)
+
+    if x != 0:
+        return x*player
+    pos = -1
+    value = -2
+
+    for i in range(0,9):
+        if board[i] == 0:
+            board[i] = 1
+            score = -(minmax(board, player *-1))
+            board[i] = 0
+            if score > value:
+                value = score
+                pos = i
+
+    if pos == -1:
+        return 0
+    return value
+
+
 
 def compTurn(board):
+    pos = -1
+    value = -2
+
+    for i in range(0,9):
+        if board[i] == 0:
+            board[i] = 1
+            score = -(minmax(board, -1))
+            board[i] = 0
+            if score > value:
+                value = score
+                pos = i
+
+    board[pos] = 1
 
 def analyze_board(board):
     cb = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
